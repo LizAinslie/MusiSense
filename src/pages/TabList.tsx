9fe4960d48fb324c135f24c9fcb4cceb8d0df5e7
Landon Gravat <railinator4903@gmail.com>
2019-06-25 18:10:56 -0500

    add the ability to 'delete' songs

--- src/pages/TabList.tsx
+++ src/pages/TabList.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonAvatar, IonLabel, IonImg } from '@ionic/react';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonAvatar, IonLabel, IonImg, IonButton, IonButtons, IonIcon, IonAlert } from '@ionic/react';
 import ISong from '../interfaces/ISong';
 
 interface HomeProps { }
 interface HomeState {
   songs: ISong[];
+  editing: boolean;
+  showConfirmDelete: boolean;
+  songToDelete?: number;
 }
 
 class TabList extends React.Component<HomeProps, HomeState> {
@@ -13,66 +16,83 @@ class TabList extends React.Component<HomeProps, HomeState> {
     super(props);
 
     this.state = {
-      songs: [],
+      songs: [
+        {
+          title: 'Beastmode',
+          contributingArtist: [
+            'Teminite',
+          ],
+          album: 'Inception',
+          albumArt: 'https://i.railrunner16.me/I6oqYs6.jpeg',
+          trackNumber: 5,
+          year: 2016,
+        },
+        {
+          title: 'Inception',
+          contributingArtist: [
+            'Teminite',
+          ],
+          album: 'Inception',
+          albumArt: 'https://i.railrunner16.me/I6oqYs6.jpeg',
+          trackNumber: 1,
+          year: 2016,
+        },
+        {
+          title: 'Space Invaders',
+          contributingArtist: [
+            'Teminite',
+            'MDK',
+          ],
+          album: 'Space Invaders - Single',
+          trackNumber: 1,
+          year: 2019,
+        },
+        {
+          title: 'Highscore',
+          contributingArtist: [
+            'Panda Eyes',
+          ],
+          featuredArtist: [
+            'Teminite',
+          ],
+          album: 'KIKO',
+          trackNumber: 5,
+          year: 2015,
+        },
+      ],
+      editing: false,
+      showConfirmDelete: false,
     };
   }
 
-  public render() {
-    const songs: ISong[] = [
-      {
-        title: 'Beastmode',
-        contributingArtist: [
-          'Teminite',
-        ],
-        album: 'Inception',
-        albumArt: 'https://i.railrunner16.me/I6oqYs6.jpeg',
-        trackNumber: 5,
-        year: 2016,
-      },
-      {
-        title: 'Inception',
-        contributingArtist: [
-          'Teminite',
-        ],
-        album: 'Inception',
-        albumArt: 'https://i.railrunner16.me/I6oqYs6.jpeg',
-        trackNumber: 1,
-        year: 2016,
-      },
-      {
-        title: 'Space Invaders',
-        contributingArtist: [
-          'Teminite',
-          'MDK',
-        ],
-        album: 'Space Invaders - Single',
-        trackNumber: 1,
-        year: 2019,
-      },
-      {
-        title: 'Highscore',
-        contributingArtist: [
-          'Panda Eyes',
-        ],
-        featuredArtist: [
-          'Teminite',
-        ],
-        album: 'KIKO',
-        trackNumber: 5,
-        year: 2015,
-      },
-    ];
+  private removeSong(index: number) {
+    const arr: ISong[] = [...this.state.songs];
+    arr.splice(index, 1);
+    this.setState({
+      songs: arr,
+    });
+  }
 
+  public render() {
     return (
       <>
         <IonHeader>
           <IonToolbar>
             <IonTitle>List</IonTitle>
+            <IonButtons slot='primary'>
+              <IonButton onClick={() => {
+                this.setState((previousState, props) => ({
+                  editing: !previousState.editing,
+                }));
+              }}>
+                {this.state.editing ? "Done" : "Edit"}
+              </IonButton>
+            </IonButtons>
           </IonToolbar>
         </IonHeader>
         <IonContent>
           <IonList>
-            {songs.map((song: ISong) => {
+            {this.state.songs.map((song: ISong, index: number) => {
               return (
                 <IonItem>
                   <IonAvatar slot='start'>
@@ -87,10 +107,44 @@ class TabList extends React.Component<HomeProps, HomeState> {
                     ) : ''}</h2>
                     <p>{song.contributingArtist.join(', ')} &mdash; {song.album}</p>
                   </IonLabel>
+                  {this.state.editing ? (
+                    <IonButton slot='end' fill='clear' onClick={() => {
+                      this.setState({
+                        songToDelete: index,
+                        showConfirmDelete: true,
+                      });
+                    }}>
+                      <IonIcon name='remove' />
+                    </IonButton>
+                  ) : ''}
                 </IonItem>
               );
             })}
           </IonList>
+          <IonAlert
+            isOpen={this.state.showConfirmDelete}
+            onDidDismiss={() => {
+              this.setState({
+                showConfirmDelete: false,
+              });
+            }}
+            header={'Confirm Delete'}
+            subHeader={'Are you sure you want to delete this song?'}
+            message={'This will permanently remove this song from your library. You won\'t be able to get it back.'}
+            buttons={[
+              {
+                text: 'Cancel',
+                role: 'cancel',
+                handler: () => { },
+              },
+              {
+                text: 'Delete',
+                handler: () => {
+                  this.removeSong(this.state.songToDelete!);
+                }
+              },
+            ]}
+          ></IonAlert>
         </IonContent>
       </>
     );
